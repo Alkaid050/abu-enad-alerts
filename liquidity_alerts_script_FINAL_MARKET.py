9778850b81e4b2f75fd003a24fed006cdeedf83c
Alkaid050 <hzwm.njd@gmail.com>
2025-06-13 21:42:49 +0300

    Update liquidity_alerts_script_FINAL_MARKET.py

--- liquidity_alerts_script_FINAL_MARKET.py
+++ liquidity_alerts_script_FINAL_MARKET.py
@@ -58,7 +58,7 @@ def process_stock(symbol):
     if not sent_signals[symbol]["entry"] and volume > 50000 and change >= 2:
         expected = calculate_expected_price(current_price, volume / 1000)
         fair_value = round(expected * 1.1, 2)
-       message = f"🚨 <b>دخول سيولة</b>\n📈 <b>{symbol}</b>\n💰 السعر الحالي: <b>{current_price}</b>\n⏰ {timestamp}"
+       message =f"🚨 <b>دخول سيولة</b>\n📈 <b>{symbol}</b>\n💰 السعر الحالي: <b>{current_price}</b>\n⏰ {timestamp}"
         send_telegram_message(CHANNEL_1, msg)
         sent_signals[symbol]["entry"] = True
         peak_liquidity[symbol] = volume
